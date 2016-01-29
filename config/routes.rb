3c40c075a8f3840efa86ae73a8af6b9f944003be
Skitelman <sghitelman@gmail.com>
2016-01-29 15:11:17 -0500

    create pets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :pets, only: [:index, :new, :create, :show, :edit, :update]
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:show]
   root "welcome#index"
