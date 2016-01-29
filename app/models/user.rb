c865dbf5c47e5c935f86fcdacf28613b249fa732
Skitelman <sghitelman@gmail.com>
2016-01-29 10:09:55 -0500

    create user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :ownerships, :foreign_key => "owner_id"
   has_many :pets, through: :ownerships
   has_many :requests, :foreign_key => "owner_id"
   has_many :walks, :foreign_key => "walker_id"
+
+  def name
+    self.first_name + " " + self.last_name
+  end
 end
---
+++ db/migrate/20160129150213_remove_passowrd_digest_from_users.rb
@@ -0,0 +1,5 @@
+class RemovePassowrdDigestFromUsers < ActiveRecord::Migration
+  def change
+    rename_column :users, :passowrd_digest, :password_digest
+  end
+end
