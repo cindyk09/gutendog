36f3e345b77de90d2a7278b4f53748f62769c710
cindyk09 <cindy_kim88@hotmail.com>
2016-01-29 11:06:17 -0500

    so far devise gem is working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :ownerships, :foreign_key => "owner_id"
   has_many :pets, through: :ownerships
   has_many :requests, :foreign_key => "owner_id"
