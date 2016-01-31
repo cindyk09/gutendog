a9ed27850eb630c03995ef33b90ae2e4055c6b50
cindyk09 <cindy_kim88@hotmail.com>
2016-01-30 19:26:25 -0500

    created requests and ability to request walks

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,14 +1,27 @@
 class RequestsController < ApplicationController
   def new
-    @request = Request.new
+    @request = Request.new(start_time: DateTime.now, end_time: DateTime.now)
   end
 
   def create
+    @request = Request.new(request_params)
+    @request.owner = current_user
+    if @request.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
+    @request = Request.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def request_params
+    params.require(:request).permit(:start_time, :end_time, :pickup)
   end
 end
