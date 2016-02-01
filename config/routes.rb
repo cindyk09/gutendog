968306f550572ed545de2be2c858fb00ff05b677
cindyk09 <cindy_kim88@hotmail.com>
2016-02-01 14:41:12 -0500

    added open request page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :walks, only: [:show, :create, :update]
-  resources :requests, only: [:new, :create, :show, :edit]
+  resources :requests, only: [:index, :new, :create, :show, :edit]
   resources :pets, only: [:index, :new, :create, :show, :edit, :update]
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:show, :index]
