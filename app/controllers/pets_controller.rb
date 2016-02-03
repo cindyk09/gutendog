1db3ee1b794077168b5d71481dd91fcc025aebfd
Skitelman <sghitelman@gmail.com>
2016-02-03 11:01:00 -0500

    pictures work locally

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -34,6 +34,6 @@ class PetsController < ApplicationController
 
   private
   def pet_params
-    params.require(:pet).permit(:name, :description)
+    params.require(:pet).permit(:name, :description, :image)
   end
 end
