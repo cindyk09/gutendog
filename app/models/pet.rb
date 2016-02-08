18aa13c8d2d7a5e21af59f7ef70526ea62ea2745
Kevin Mircovich <kmircovich1@gmail.com>
2016-02-08 09:53:51 -0500

    pets index and show and able to edit pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,4 +4,13 @@ class Pet < ActiveRecord::Base
   has_many :petwalks, dependent: :destroy
   has_many :walks, through: :petwalks
   mount_uploader :image, ImageUploader
+
+  def truncated_description
+    if description.length > 214
+      description[0..170] + "..."
+    else
+      description
+    end
+    
+  end
 end
