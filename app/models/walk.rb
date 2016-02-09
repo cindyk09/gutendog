5df760a0ea1659a2bed58c0bb07b8d08a631aa33
Skitelman <sghitelman@gmail.com>
2016-02-09 10:13:49 -0500

    added dependent destroy to walk petwalks

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -1,6 +1,6 @@
 class Walk < ActiveRecord::Base
   belongs_to :walker, class_name: "User"
   belongs_to :request
-  has_many :petwalks
+  has_many :petwalks, dependent: :destroy
   has_many :pets, through: :petwalks
 end
