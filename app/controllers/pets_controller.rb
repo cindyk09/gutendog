c4c266c7bb667eda42788570bf343e157ea0a19d
cindyk09 <cindy_kim88@hotmail.com>
2016-12-24 15:20:16 -0500

    refactored the method to find pets_id

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,7 @@
 class PetsController < ApplicationController
   before_action :authenticate_user!
+  before_action :search_pet, only: [:edit, :show, :update, :destroy]
+
   def index
     @pets = current_user.pets
   end
@@ -19,20 +21,34 @@ class PetsController < ApplicationController
   end
 
   def show
-    @pet = Pet.find(params[:id])
+    # @pet = current_user.pets.find(params[:id])
   end
 
   def edit
-    @pet = Pet.find(params[:id])
+    # @pet = Pet.find(params[:id])
   end
 
   def update
-    @pet = Pet.find(params[:id])
+    # @pet = Pet.find(params[:id])
     @pet.update(pet_params)
     redirect_to @pet
   end
 
+  # def destroy
+  # @pet.destroy
+  #   respond_to do |format|
+  #     # confirm this redirect works
+  #     format.html {user_path(current_user)}
+  #     format.js { }
+  #   end
+  # end
+
   private
+  def search_pet
+    @pet = current_user.pets.find(params[:id])
+    # @pet = Pet.find(params[:id])
+  end
+
   def pet_params
     params.require(:pet).permit(:name, :description, :image, :remote_image_url)
   end
